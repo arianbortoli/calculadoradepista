cef38b62dedef4aafea79d67bf3d3ed39d6606a5
arianbortoli <arianbortoli@github.com>
2025-11-28 11:04:15 -0300

    feat: add Vercel Analytics and Speed Insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default MyApp;
